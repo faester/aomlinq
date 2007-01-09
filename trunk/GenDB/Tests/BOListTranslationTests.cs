e675a74bc4608b34536ce07e2d755b3e6b93dbf7
faester <faester@ba409d57-2b21-0410-a533-5f1d92688413>
2007-01-09 10:04:31 +0000

    TestPerson rykket til egen fil.

--- trunk/GenDB/Tests/BOListTranslationTests.cs
+++ trunk/GenDB/Tests/BOListTranslationTests.cs
@@ -6,32 +6,7 @@ using GenDB;
 
 namespace Tests
 {
-    class TestPerson : AbstractBusinessObject
-    {
-        string name;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        int age;
-
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-
-        TestPerson spouse;
-
-        internal TestPerson Spouse
-        {
-            get { return spouse; }
-            set { spouse = value; }
-        }
-    }
+  
 
     [TestFixture]
     public class BOListTranslationTests
