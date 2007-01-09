95d96cfe8d1815864abe3d3f6de5e84734b1aa52
faester <faester@ba409d57-2b21-0410-a533-5f1d92688413>
2007-01-09 10:04:31 +0000

    TestPerson rykket til egen fil.

---
+++ GenDB/Tests/TestPerson.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GenDB;
+
+namespace Tests
+{
+    class TestPerson : AbstractBusinessObject
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        TestPerson spouse;
+
+        internal TestPerson Spouse
+        {
+            get { return spouse; }
+            set { spouse = value; }
+        }
+    }
+}
