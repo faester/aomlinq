192ec7854dd17e27264b40c93d429ec1f9d7ca45
faester <faester@ba409d57-2b21-0410-a533-5f1d92688413>
2007-02-28 01:15:19 +0000

    Kommentarer

--- trunk/GenDB/GenDB/GenDBAnnotations.cs
+++ trunk/GenDB/GenDB/GenDBAnnotations.cs
@@ -4,11 +4,19 @@ using System.Text;
 
 namespace GenDB
 {
+    /// <summary>
+    /// Indicates that the user does not want this property to 
+    /// be persisted, even though it is public and has both a 
+    /// getter and a setter.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class Volatile : Attribute { }
 
     /// <summary>
-    /// Indicates that the user
+    /// Indicates that the user wants the property to be 
+    /// retrieved lazyly. The constructor takes a string 
+    /// naming the field LazyLoader<typeparamref name="T"/>
+    /// that should hold the properties value.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class LazyLoad : Attribute
