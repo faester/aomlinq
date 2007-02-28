6d8b9e22169b1fc472cf7e940668a0fa4f51b85b
faester <faester@ba409d57-2b21-0410-a533-5f1d92688413>
2007-02-28 01:14:51 +0000

    Kommentarer...

--- GenDB/GenDB/DataContext.cs
+++ GenDB/GenDB/DataContext.cs
@@ -6,6 +6,11 @@ using System.Data.SqlClient;
 
 namespace GenDB
 {
+    /// <summary>
+    /// The main entry point to the persistence framework. 
+    /// Use the datacontext to set database name and obtain 
+    /// new instances of Table&lt;T&gt;-objects.
+    /// </summary>
     public class DataContext
     {
         static DataContext instance = new DataContext();
@@ -17,6 +22,13 @@ namespace GenDB
             get { return isInitialized; }
         }
 
+        /// <summary>
+        /// Returns a table capeable of holding 
+        /// objects of type T. First call to this method 
+        /// will initilize the DataContext.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public Table<T> GetTable<T> ()
             where T : IBusinessObject, new()
         {
@@ -28,6 +40,9 @@ namespace GenDB
         }
 
 
+        /// <summary>
+        /// The instance of the DataContext. 
+        /// </summary>
         public static DataContext Instance
         {
             get { return DataContext.instance; }
@@ -68,6 +83,13 @@ namespace GenDB
             genDB = new MsSql2005DB (this);
         }
 
+
+        /// <summary>
+        /// Initializes DataContext. This implies that class descriptions 
+        /// are loaded internally and the entire framework is prepared for 
+        /// usage. Prior to calling this method it should be ensured that 
+        /// the underlying database has been created etc. 
+        /// </summary>
         public void Init()
         {
             if (isInitialized)
@@ -90,7 +112,8 @@ namespace GenDB
         /// Attempts to build database. All changes will be lost. 
         /// Cache will be emptied without committing. Should only 
         /// be used as part of program initialization before any-
-        /// thing has been added to the database.
+        /// thing has been added to the database. Must only be called 
+        /// when DataContext is still uninitialized.
         /// </summary>
         public void DeleteDatabase()
         {
@@ -105,7 +128,8 @@ namespace GenDB
         /// <summary>
         /// Attempts to build database. All changes will be lost. 
         /// Cache will be emptied. Should only be used as part 
-        /// of program initialization.
+        /// of program initialization. Must only be called 
+        /// when DataContext is still uninitialized.
         /// </summary>
         public void CreateDatabase()
         {
@@ -116,11 +140,22 @@ namespace GenDB
             genDB.CreateDatabase();
         }
 
+        /// <summary>
+        /// Returns true if database exists. 
+        /// </summary>
+        /// <returns></returns>
         public bool DatabaseExists()
         {
             return genDB.DatabaseExists();
         }
 
+        /// <summary>
+        /// When the internal database sends commands (inserts/deletes/updates)
+        /// to the database, it will concatenate a number of query strings and 
+        /// send them as one batch. This number represents the batch size.
+        /// Note that changes will only take effect for future database operations.
+        /// The value should probably not be changed by the user. 
+        /// </summary>
         public int DbBatchSize
         {
             get { return dbBatchSize; }
@@ -129,6 +164,10 @@ namespace GenDB
 
         private int commandTimeout = 0;
 
+        /// <summary>
+        /// Sets the TimeOut of commands created internally in the framework. 
+        /// It will probably be a good idea to leave this unchanged.
+        /// </summary>
         public int CommandTimeout
         {
             get { return commandTimeout; }
@@ -137,11 +176,21 @@ namespace GenDB
 
         string connectStringWithDBName = "server=(local);database=generic;Integrated Security=SSPI;connection timeout=0";
 
+
+        /// <summary>
+        /// Returns a closed connection to the database.
+        /// </summary>
+        /// <returns></returns>
         internal SqlConnection CreateDBConnection()
         {
             return new SqlConnection(connectStringWithDBName);
         }
 
+        /// <summary>
+        /// Will create a connection to the server. The connection will not be opened, and no 
+        /// database name is set.
+        /// </summary>
+        /// <returns></returns>
         internal SqlConnection CreateServerConnection()
         {
             return new SqlConnection(connectStringWithoutDBName);
@@ -151,20 +200,33 @@ namespace GenDB
 
         string dbname = "generic";
 
+        /// <summary>
+        /// Sets the database name. An exception will be thrown if 
+        /// the name is changed after DataContext has been initialized.
+        /// </summary>
         public string DatabaseName 
         {
             get { return dbname; }
-            set { 
+            set {
+                if (IsInitialized) { throw new Exception("Database name can not be changed when the datacontext has been initialized."); }
                 dbname = value; 
                 connectStringWithDBName = connectStringWithoutDBName + ";database=" + value;
             }
         }
 
+
+        /// <summary>
+        /// Will submit newly added as well as changed object to the database.
+        /// </summary>
         public void SubmitChanges()
         {
             iboCache.SubmitChanges();
         }
 
+        /// <summary>
+        /// All objects added since last commit will be removed from the cache and thus
+        /// not added to the database. (Unless they are referenced by some other persisted object.)
+        /// </summary>
         public void RollbackTransaction()
         {
             iboCache.RollbackTransaction();
